255beb90560e7ef46bbfa5e9f7fb6e873272f3cd
a111182116-beep <a111182116@nkust.edu.tw>
2026-05-09 01:38:54 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,10 @@ import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
 
 export default defineConfig(({mode}) => {
-  const env = loadEnv(mode, '.', '');
+  const env = loadEnv(mode, process.cwd(), '');
+  
   return {
-    base： '/1234/'，
+    base： '/1234/',
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
